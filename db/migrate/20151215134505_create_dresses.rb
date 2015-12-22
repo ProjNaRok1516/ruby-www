c1b715c99425473009e566d26b76f3414a1522a5
MKlemarczyk <m.klemarczyk@live.com>
2015-12-22 13:43:46 +0100

    Poprawiony plik migracji dla tabeli z sukienkami

--- db/migrate/20151215134505_create_dresses.rb
+++ db/migrate/20151215134505_create_dresses.rb
@@ -1,11 +1,11 @@
 class CreateDresses < ActiveRecord::Migration
   def change
-    create_table :desses do |t|
+    create_table :dresses do |t|
       t.string :name
       t.decimal :price_per_m2
     end
-	add_reference :desses, :order, index: true, foreign_key: true
-	add_reference :style, :order, index: true, foreign_key: true
-	add_reference :material, :order, index: true, foreign_key: true
+	add_reference :dresses, :order, index: true, foreign_key: true
+	add_reference :dresses, :style, index: true, foreign_key: true
+	add_reference :dresses, :material, index: true, foreign_key: true
   end
 end
