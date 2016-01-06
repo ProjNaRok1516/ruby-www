b65a8010081c85323b63f55a6b8e349a2ee386c0
zwojcicka <zuzia.wojcicka@gmail.com>
2016-01-06 17:20:17 +0100

    Update order.rb

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,7 +1,7 @@
 class Order < ActiveRecord::Base
   belongs_to :client
   validates :data, :total_cost, :status, presence: true
-  validates :total_cost, numericality: { greater_than: 0, less_than: 500000 }
+  validates :total_cost, numerality: { greater_than: 0, less_than: 500000 }
   validates :status, numerality: {greater_than_or_equal_to: 0, less_than_or_equal_to: 1, only_integer: true}
   validates :data, format: {with: /[0-3][0-9]\-[0-1][0-9]\-[2-9][0-9]{3}/}
   validates_associated :client
