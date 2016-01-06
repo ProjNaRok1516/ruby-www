15080e6354aecf0557edcabe10a675be5289ae6e
zwojcicka <zuzia.wojcicka@gmail.com>
2016-01-06 17:09:19 +0100

    Update client.rb

--- app/models/client.rb
+++ app/models/client.rb
@@ -4,6 +4,7 @@ class Client < ActiveRecord::Base
    validates :firstname, :lastname, :phone, presence: true
    validates :firstname, :lastname, length: { minimum: 3, maximum: 50 } 
    validates :phone, length: { is: 9 }
+   validates :phone, format: { with: [1-9][0-9]{8}}
    validates :firstname, :lastname, format:  { with: /[A-Z][a-z]*/, message: "allows only capital letter at the beggining" }
    
   
