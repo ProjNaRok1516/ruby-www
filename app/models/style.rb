803cc26c6d00516b831452d8d78f1658a826e6ea
zwojcicka <zuzia.wojcicka@gmail.com>
2016-01-06 16:54:44 +0100

    Update style.rb

--- app/models/style.rb
+++ app/models/style.rb
@@ -2,5 +2,6 @@ class Style < ActiveRecord::Base
   validates :name,:image,:material_amout, presence: true
   validates :name, length: { minimum: 3, maximum:50 }
   validates :material_amout, numericality: { greater_than:0, less_than:5 }
+  validates :image, format: {with:  /.\.(png|jpg}/ message: "allows only png or jpg extension"  }
   
 end
