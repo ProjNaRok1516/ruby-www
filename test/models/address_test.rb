054a00ed64f7bf722c5441d7a7f365c872d9a229
AdriannaPopko <adunia_93@op.pl>
2016-01-06 13:01:01 +0100

    Update address_test.rb
    Dodano testy poprawności adresu.

--- test/models/address_test.rb
+++ test/models/address_test.rb
@@ -1,7 +1,28 @@
 require 'test_helper'
 
 class AddressTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "the truth" do
+    assert true
+  end
+  test "should not save wrong address" do
+    adres = Address.new
+    assert_not adres.save
+  end
+  test "should save correct address" do
+    adres = Address.new
+    adres.street = "Grunwaldzka"
+    adres.building = "2"
+    adres.flat = "2"
+    adres.city = "Gdansk"
+    adres.postalcode = "80-180"
+    assert adres.save
+  end
+  test "should save correct address without flat" do
+    adres = Address.new
+    adres.street = "Grunwaldzka"
+    adres.building = "2"
+    adres.city = "Gdansk"
+    adres.postalcode = "80-180"
+    assert adres.save
+  end
 end
