e30025b7f70c34b1b7d55c32a170f5ee37adb7b3
AdriannaPopko <adunia_93@op.pl>
2016-01-06 14:23:50 +0100

    Update address_test.rb
    Zastosowanie fixtures.

--- test/models/address_test.rb
+++ test/models/address_test.rb
@@ -1,28 +1,97 @@
 require 'test_helper'
 
 class AddressTest < ActiveSupport::TestCase
-  test "the truth" do
-    assert true
-  end
-  test "should not save wrong address" do
-    adres = Address.new
-    assert_not adres.save, "empty address saved"
-  end
   test "should save correct address" do
-    adres = Address.new
-    adres.street = "Grunwaldzka"
-    adres.building = "2"
-    adres.flat = "2"
-    adres.city = "Gdansk"
-    adres.postalcode = "80-180"
-    assert adres.save, "full address not saved"
+    adres = addresses(:one)
+    assert adres.save, "correct address not saved"
   end
-  test "should save correct address without flat" do
-    adres = Address.new
-    adres.street = "Grunwaldzka"
-    adres.building = "2"
-    adres.city = "Gdansk"
-    adres.postalcode = "80-180"
-    assert adres.save, "address without flat not saved"
+
+  test "should save correct address" do
+    adres = addresses(:address_without_flat)
+    assert adres.save, "correct address not saved"
   end
-end
+
+  test "should save correct address" do
+    adres = addresses(:address_without_street)
+    assert_not adres.save, "correct address not saved"
+  end
+
+  test "should save correct address" do
+    adres = addresses(:address_without_building)
+    assert_not adres.save, "correct address not saved"
+  end
+
+  test "should save correct address" do
+    adres = addresses(:address_without_city)
+    assert_not adres.save, "correct address not saved"
+  end
+
+  test "should save correct address" do
+    adres = addresses(:address_without_postalcode)
+    assert_not adres.save, "correct address not saved"
+  end
+
+  test "should save correct address" do
+    adres = addresses(:address_with_empty_street)
+    assert_not adres.save, "correct address not saved"
+  end
+
+  test "should save correct address" do
+    adres = addresses(:address_street_too_short)
+    assert_not adres.save, "correct address not saved"
+  end
+
+  test "should save correct address" do
+    adres = addresses(:address_street_too_long)
+    assert_not adres.save, "correct address not saved"
+  end
+
+  test "should save correct address" do
+    adres = addresses(:address_with_empty_building)
+    assert_not adres.save, "correct address not saved"
+  end
+
+  test "should save correct address" do
+    adres = addresses(:address_building_too_long)
+    assert_not adres.save, "correct address not saved"
+  end
+
+  test "should save correct address" do
+    adres = addresses(:address_flat_too_long)
+    assert_not adres.save, "correct address not saved"
+  end
+
+  test "should save correct address" do
+    adres = addresses(:address_with_empty_city)
+    assert_not adres.save, "correct address not saved"
+  end
+
+  test "should save correct address" do
+    adres = addresses(:address_city_too_short)
+    assert_not adres.save, "correct address not saved"
+  end
+
+  test "should save correct address" do
+    adres = addresses(:address_city_too_long)
+    assert_not adres.save, "correct address not saved"
+  end
+
+  test "should save correct address" do
+    adres = addresses(:address_postalcode_too_short)
+    assert_not adres.save, "correct address not saved"
+  end
+
+  test "should save correct address" do
+    adres = addresses(:address_postalcode_too_long)
+    assert_not adres.save, "correct address not saved"
+  end
+
+  test "should save correct address" do
+    adres = addresses(:address_postalcode_without_dash)
+    assert_not adres.save, "correct address not saved"
+  end
+
+  test "should save correct address" do
+    adres = addresses(:address_postalcode_with_letter)
+    assert_not adres.save, "correct address not saved"
+  end
