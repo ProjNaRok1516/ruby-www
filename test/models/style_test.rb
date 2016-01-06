e8dcfa05716c2755ca50daec987c1c036755b9c1
AdriannaPopko <adunia_93@op.pl>
2016-01-06 17:32:20 +0100

    Update style_test.rb
    Dodano testy dla stylu.

--- test/models/style_test.rb
+++ test/models/style_test.rb
@@ -1,35 +1,83 @@
 require 'test_helper'
 
 class StyleTest < ActiveSupport::TestCase
-  test "the truth" do
-   assert true
-   end
-   
-   test "name can't be blank" do
-    assert_not Style.create(name: "").valid?
-  end
-  test "should save name" do
-    assert Style.create(name: "Do kostek").valid?
-  end
-  test "name can't longer than 50 chars" do
-    assert_not Style.create(name: "blablafwethwyjrukjtyrhgfdcscdsfvbgnmhngbfvdsahbyjnbvdfsdferghtjnbvcsxsdwrjbvfyhtsrxcvbjvhcxreartfynknbjvhcgeththbvcserhtbfvcserghbfvsfgrfb").valid?
-  end
-  
-  test "shoud not save style's name with int value" do
-    assert_not Style.create(name: 1234).valid?
-  end
-  
-    test "shoud not save material's amout with negative value" do
-    assert_not Material.create(name: -123).valid?
-  end
-   test "shoud not save material's amout with a zero value" do
-    assert Material.create(name: 0).valid?
-  end
-  
-  test "shoud not save material's amout with a nil value" do
-    assert_not Material.create(name: nil).valid?
-  end
-  
+  test "should save correct style" do 
+   style = styles(:one) 
+   assert style.save, "correct style not saved" 
+  end 
 
-   
+  test "should save correct style with max material amount" do 
+   style = styles(:two) 
+   assert style.save, "correct style with max matreial amount not saved" 
+  end 
+  
+  test "should not save style with empty name" do 
+   style = styles(:style_with_empty_name) 
+   assert_not style.save, "style with empty name saved" 
+  end 
+
+  test "should not save style with empty image" do 
+   style = styles(:style_with_empty_image) 
+   assert_not style.save, "style with empty image saved" 
+  end 
+  
+  test "should not save style with empty material amount" do 
+   style = styles(:style_with_empty_material_amount) 
+   assert_not style.save, "style with empty material amount saved" 
+  end 
+  
+  test "should not save style without name" do 
+   style = styles(:style_without_name) 
+   assert_not style.save, "style without name saved" 
+  end 
+  
+  test "should not save style without image" do 
+   style = styles(:style_without_image) 
+   assert_not style.save, "style without image saved" 
+  end 
+  
+  test "should not save style without material amount" do 
+   style = styles(:style_without_material_amount) 
+   assert_not style.save, "style without material amount saved"
+  end 
+  
+  test "should not save style with too long name" do 
+   style = styles(:style_with_too_long_name) 
+   assert_not style.save, "style with too long name saved" 
+  end 
+  
+  test "should not save style with too big material amount" do 
+   style = styles(:style_with_too_big_material_amount) 
+   assert_not style.save, "stylewith too big material amount saved" 
+  end 
+  
+  test "should not save style with too big image name" do 
+   style = styles(:style_with_too_big_image_name) 
+   assert_not style.save, "style with too big image name saved" 
+  end 
+  
+  test "should not save style with too short name" do 
+   style = styles(:style_with_too_short_name) 
+   assert_not style.save, "style with too short name saved" 
+  end 
+  
+  test "should not save style with zero material amount" do 
+   style = styles(:style_with_zero_material_amount) 
+   assert_not style.save, "style with zero material amount saved" 
+  end 
+  
+  test "should not save style with too small image name" do 
+   style = styles(:style_with_too_small_image_name) 
+   assert_not style.save, "style with too small image name saved" 
+  end 
+  
+  test "should not save style without extension" do 
+   style = styles(:style_image_without_extension) 
+   assert_not style.save, "style image without extension saved" 
+  end 
+  
+  test "should not save style without dot before extension" do 
+   style = styles(:style_image_without_dot_before_extension) 
+   assert_not style.save, "style image without dot before extension saved" 
+  end 
 end
