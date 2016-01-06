d1e1cc302143c1987f1f95e4df5bf38db597d73a
zwojcicka <zuzia.wojcicka@gmail.com>
2016-01-06 13:47:59 +0100

    update tests

--- test/models/dress_test.rb
+++ test/models/dress_test.rb
@@ -9,23 +9,61 @@ class DressTest < ActiveSupport::TestCase
     assert_not Dress.create(name: "").valid?
   end
   
+  test "name should be saved" do
+    assert Dress.create(name: "Mala czarna").valid?
+  end
+
+  
   test "should not save dress with too short name" do
     assert_not Dress.create(name: "a").valid?
   end
-  
+ 
+ 
   test "should not save dress with an int in name" do
       assert_not Dress.create(name: "123").valid?
   end
-  
   
    test "should not save dress with a string in cost" do
      assert_not Dress.create(cost: "abcd").valid?
   end
   
-     test "should not save dress with a negative number" do
-     assert_not Dress.create(cost: "").valid?
+   test "should  save dress with a decimal in cost" do
+     assert Dress.create(cost: "200.0").valid?
   end
   
+     test "should not save dress with a negative number" do
+     assert_not Dress.create(cost: -1).valid?
+  end
    
+  
+  test "should not save dress with a zero value in cost" do
+     assert_not Dress.create(cost: 0).valid?
+  end
+  
+  test "cost can't be blank" do
+     assert_not Dress.create(cost:nil).valid?
+  end
+  
+  test "should not save dress with too big value in cost" do
+     assert_not Dress.create(cost: 1000000.0).valid?
+  end
+  
+    test "should not save dress without style" do
+     assert_not Dress.create(style: nil).valid?
+  end
+   test "should save dress with style" do
+     assert Dress.create(style: bufiasta).valid?
+  end
+  
+   test "should not save dress without material" do
+     assert_not Dress.create(style: nil).valid?
+  end
+  
+   test "should save dress with material" do
+     assert_not Dress.create(style: jedwab).valid?
+  end
+  
+  
+  
    
 end
