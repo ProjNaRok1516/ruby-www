3ac910045be0d463ccffec6bcf3e920733fc596c
zwojcicka <zuzia.wojcicka@gmail.com>
2016-01-06 15:13:26 +0100

    Update address.rb

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,11 +1,11 @@
 class Address < ActiveRecord::Base
   validates :street, :building, :city, :postalcode, presence: true
-   validates :street, :city length: { minimum: 2 }    
-   validates :street,:city length: { maximum: 50 }
+   validates :street, :city, length: { minimum: 2 }    
+   validates :street,:city, length: { maximum: 50 }
    validates :building, length: { minimum: 1 }    
-   validates :building length: { maximum: 20 }
-   validates :postalcode length: { minimum: 5 }    
-   validates :postalcode length: { maximum: 5 }  
-   validates :postalcode format: { with: /[0-9]{2}-[0-9]{3}/,
+   validates :building, length: { maximum: 20 }
+   validates :postalcode, length: { minimum: 5 }    
+   validates :postalcode, length: { maximum: 5 }  
+   validates :postalcode, format: { with: /[0-9]{2}-[0-9]{3}/,
    message: "only supports format xx-xxx" }
 end
