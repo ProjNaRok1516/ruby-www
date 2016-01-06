5be62df53f91d1dba4c27f7d9ee711b9c054d729
zwojcicka <zuzia.wojcicka@gmail.com>
2016-01-06 17:38:18 +0100

    Update material.rb

--- app/models/material.rb
+++ app/models/material.rb
@@ -1,6 +1,6 @@
 class Material < ActiveRecord::Base
   validates :name, :price, presence: true
   validates :name, length: { minimum: 3, maximum: 50 }
-  validates :price, numerality: {greater_than: 0, less_than: 1000 }
+  validates :price, numericality: {greater_than: 0, less_than: 1000 }
   
 end
