0e2478ad3896fa8aacf84a33119ec8fdf8f710d2
zwojcicka <zuzia.wojcicka@gmail.com>
2016-01-06 16:23:32 +0100

    Update style.rb

--- app/models/style.rb
+++ app/models/style.rb
@@ -1,5 +1,5 @@
 class Style < ActiveRecord::Base
-  validates :name,:image,:material_amout presence: true
+  validates :name,:image,:material_amout, presence: true
   validates :name, length: { minimum: 3, maximum:50 }
   validates :material_amout, length: { minimum: 1, maximum:5 }
   
