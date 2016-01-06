895f975bf47171213c486e91f0fc7f9b8a28309c
AdriannaPopko <adunia_93@op.pl>
2016-01-06 14:57:15 +0100

    Update address_test.rb
    Dodano komentarze do testow.

--- test/models/address_test.rb
+++ test/models/address_test.rb
@@ -11,87 +11,88 @@ class AddressTest < ActiveSupport::TestCase
     assert adres.save, "correct address not saved"
   end
 
-  test "should save correct address" do
+  test "should not save address without street" do
     adres = addresses(:address_without_street)
-    assert_not adres.save, "correct address not saved"
+    assert_not adres.save, "address saved without street"
   end
 
-  test "should save correct address" do
+  test "should not save address without building" do
     adres = addresses(:address_without_building)
-    assert_not adres.save, "correct address not saved"
+    assert_not adres.save, "address saved without building"
   end
 
-  test "should save correct address" do
+  test "should not save address without city" do
     adres = addresses(:address_without_city)
-    assert_not adres.save, "correct address not saved"
+    assert_not adres.save, "address saved without city"
   end
 
-  test "should save correct address" do
+  test "should not save address without postalcode" do
     adres = addresses(:address_without_postalcode)
-    assert_not adres.save, "correct address not saved"
+    assert_not adres.save, "address saved without postalcode"
   end
 
-  test "should save correct address" do
+  test "should not save address with empty street" do
     adres = addresses(:address_with_empty_street)
-    assert_not adres.save, "correct address not saved"
+    assert_not adres.save, "address saved with empty street"
   end
 
-  test "should save correct address" do
+  test "should not save address with too short street" do
     adres = addresses(:address_street_too_short)
-    assert_not adres.save, "correct address not saved"
+    assert_not adres.save, "address saved with too short street"
   end
 
-  test "should save correct address" do
+  test "should not save address with too long street" do
     adres = addresses(:address_street_too_long)
-    assert_not adres.save, "correct address not saved"
+    assert_not adres.save, "address saved with too long street"
   end
 
-  test "should save correct address" do
+  test "should not save address with empty building" do
     adres = addresses(:address_with_empty_building)
-    assert_not adres.save, "correct address not saved"
+    assert_not adres.save, "address saved with empty building"
   end
 
-  test "should save correct address" do
+  test "should not save address with too long building number" do
     adres = addresses(:address_building_too_long)
-    assert_not adres.save, "correct address not saved"
+    assert_not adres.save, "address saved with too long building number"
   end
 
-  test "should save correct address" do
+  test "should not save address with to long flat number" do
     adres = addresses(:address_flat_too_long)
-    assert_not adres.save, "correct address not saved"
+    assert_not adres.save, "address saved with too long flat number"
   end
 
-  test "should save correct address" do
+  test "should not save address with empty city" do
     adres = addresses(:address_with_empty_city)
-    assert_not adres.save, "correct address not saved"
+    assert_not adres.save, "address saved with empty city"
   end
 
-  test "should save correct address" do
+  test "should not save address with too short city name" do
     adres = addresses(:address_city_too_short)
-    assert_not adres.save, "correct address not saved"
+    assert_not adres.save, "address saved with too short city name"
   end
 
-  test "should save correct address" do
+  test "should not save address with too long city name" do
     adres = addresses(:address_city_too_long)
-    assert_not adres.save, "correct address not saved"
+    assert_not adres.save, "address saved with too long city name"
   end
 
-  test "should save correct address" do
+  test "should not save address with too short postalcode" do
     adres = addresses(:address_postalcode_too_short)
-    assert_not adres.save, "correct address not saved"
+    assert_not adres.save, "address saved with too short postalcode"
   end
 
-  test "should save correct address" do
+  test "should not save address with too long postalcode" do
     adres = addresses(:address_postalcode_too_long)
-    assert_not adres.save, "correct address not saved"
+    assert_not adres.save, "address saved with too long postalcode"
   end
 
-  test "should save correct address" do
+  test "should not save address postalcode without dash" do
     adres = addresses(:address_postalcode_without_dash)
-    assert_not adres.save, "correct address not saved"
+    assert_not adres.save, "address saved without dash in postalcode "
   end
 
-  test "should save correct address" do
+  test "should not save address postalcode with letter" do
     adres = addresses(:address_postalcode_with_letter)
-    assert_not adres.save, "correct address not saved"
+    assert_not adres.save, "address saved with letter in postalcode"
   end
+end
