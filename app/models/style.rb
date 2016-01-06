187bc15ffcb8b92cfe9c7b8975a9dadb64fad00e
Maciej Klemarczyk <m.klemarczyk+git@live.com>
2016-01-06 19:10:26 +0100

    Update style.rb
    Addes image name length limitation.

--- app/models/style.rb
+++ app/models/style.rb
@@ -1,6 +1,7 @@
 class Style < ActiveRecord::Base
   validates :name,:image,:material_amount, presence: true
   validates :name, length: { minimum: 3, maximum:50 }
+  validates :image, length: { minimum: 7, maximum:54 }
   validates :material_amount, numericality: { greater_than:0, less_than_or_equal_to:5 }
   validates :image, format: {with:  /^.*\.(png|jpg)$/, message: "allows only png or jpg extension" }
   
