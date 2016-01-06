6764698d7a0a2b2fab482981e214ed192f0d15a3
zwojcicka <zuzia.wojcicka@gmail.com>
2016-01-06 13:30:24 +0100

    update tests

--- test/models/dress_test.rb
+++ test/models/dress_test.rb
@@ -1,7 +1,31 @@
 require 'test_helper'
 
 class DressTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "the truth" do
+   assert true
+   end
+   
+   test "name can't be blank" do
+    assert_not Dress.create(name: "").valid?
+  end
+  
+  test "should not save dress with too short name" do
+    assert_not Dress.create(name: "a").valid?
+  end
+  
+  test "should not save dress with an int in name" do
+      assert_not Dress.create(name: "123").valid?
+  end
+  
+  
+   test "should not save dress with a string in cost" do
+     assert_not Dress.create(cost: "abcd").valid?
+  end
+  
+     test "should not save dress with a negative number" do
+     assert_not Dress.create(cost: "").valid?
+  end
+  
+   
+   
 end
