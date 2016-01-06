f8fd7efe639a38c9ce3bc8180685518a1ae9a1e9
zwojcicka <zuzia.wojcicka@gmail.com>
2016-01-06 17:13:18 +0100

    Update client.rb

--- app/models/client.rb
+++ app/models/client.rb
@@ -4,7 +4,7 @@ class Client < ActiveRecord::Base
    validates :firstname, :lastname, :phone, presence: true
    validates :firstname, :lastname, length: { minimum: 3, maximum: 50 } 
    validates :phone, length: { is: 9 }
-   validates :phone, format: { with: [1-9][0-9]{8}}
+   validates :phone, format: { with: /[1-9][0-9]{8}/, message: "allows only proper format od phone"}
    validates :firstname, :lastname, format:  { with: /[A-Z][a-z]*/, message: "allows only capital letter at the beggining" }
    
   
