ead945ef8d90c0637d099ad92d55826f5dcb7bdc
Maciej Klemarczyk <m.klemarczyk+git@live.com>
2016-01-06 13:38:05 +0100

    Update dress.rb
    Dodanie walidacji dla sukienki, że powinna posiadać styl, material i być zamówiona.

--- app/models/dress.rb
+++ app/models/dress.rb
@@ -2,5 +2,7 @@ class Dress < ActiveRecord::Base
   belongs_to :style
   belongs_to :material
   belongs_to :order
-  validates_associated :books
+  validates_associated :style
+  validates_associated :material
+  validates_associated :order
 end
