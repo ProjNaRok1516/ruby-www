68f3b9c582ddb0a826f3d8ff09161eef27ff4216
AdriannaPopko <adunia_93@op.pl>
2016-01-06 16:38:45 +0100

    Update material_test.rb
    Dodano testy dla materiałów.

--- test/models/material_test.rb
+++ test/models/material_test.rb
@@ -1,42 +1,58 @@
 require 'test_helper'
 
 class MaterialTest < ActiveSupport::TestCase
-   test "the truth" do
-   assert true
+  test "should save correct material" do
+    material = materials(:one)
+    assert material.save, "correct material not saved"
   end
   
-  test "name can't be blank" do
-    assert_not Material.create(name: "").valid?
-  end
-  test "should save name" do
-    assert Material.create(name: "jedwab").valid?
-  end
-  test "name can't longer than 50 chars" do
-    assert_not Material.create(name: "jedwabfwethwyjrukjtyrhgfdcscdsfvbgnmhngbfvdsahbyjnbvdfsdferghtjnbvcsxsdwreththbvcserhtbfvcserghbfvsfgrfb").valid?
+   test "should save correct material" do
+    material = materials(:two)
+    assert material.save, "correct material not saved"
   end
   
-  test "shoud not save material's name with int value" do
-    assert_not Material.create(name: 123).valid?
+   test "should not save material without name" do
+    material = materials(:material_without_name)
+    assert_not material.save, "material saved without name"
+  end
+
+  test "should not save material without price" do
+    material = materials(:material_without_price)
+    assert_not material.save, "material saved without price"
+  end
+
+  test "should not save material with empty name" do
+    material = materials(:material_with_empty_name)
+    assert_not material.save, "material saved with empty name"
   end
   
-    test "shoud not save material's cost with negative value" do
-    assert_not Material.create(name: -123).valid?
-  end
-   test "shoud save material's cost" do
-    assert Material.create(name: 25.00).valid?
+   test "should not save material with empty price" do
+    material = materials(:material_with_empty_price)
+    assert_not material.save, "material saved with empty price"
   end
   
-  test "shoud not save material's cost with nil value" do
-    assert_not Material.create(name: nil).valid?
-  end
+   test "should not save material with too short name" do
+    material = materials(:material_with_name_too_short)
+    assert_not material.save, "material saved with too short name"
+  end 
   
-  test "shoud not save material's cost with zero value" do
-    assert_not Material.create(name: 0).valid?
-  end
+   test "should not save material with too long name" do
+    material = materials(:material_with_name_too_long)
+    assert_not material.save, "material saved with too long name"
+  end 
   
-  test "shoud not save material's cost with a string value" do
-    assert_not Material.create(name: "avbfdsvrghtbfvcdgrhtbvcfghdsdfsgd").valid?
-  end
+   test "should not save material with negative price" do
+    material = materials(:material_with_negative_price)
+    assert_not material.save, "material saved with negative price"
+  end 
   
-  
+   test "should not save material no cost" do
+    material = materials(:material_with_no_cost)
+    assert_not material.save, "material saved with no cost"
+  end 
+
+   test "should not save material with too big price" do
+    material = materials(:material_with_big_price)
+    assert_not material.save, "material saved with too big price"
+  end 
 end
