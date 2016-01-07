11926927eb19c96df9480beefbd7bc425467c515
Maciej Klemarczyk <m.klemarczyk+git@live.com>
2016-01-07 15:37:32 +0100

    Update client_test.rb

--- test/models/client_test.rb
+++ test/models/client_test.rb
@@ -1,11 +1,8 @@
 require 'test_helper'
 
-
-
 class ClientTest < ActiveSupport::TestCase
   test "should save proper client" do
     client = clients(:one)
-    client.address = addresses(:one)
     assert_not client.save, "proper client not saved"
   end
 
