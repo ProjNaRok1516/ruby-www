c63c375fe86ed9265e04b6d2262329c073d73871
Maciej Klemarczyk <m.klemarczyk+git@live.com>
2016-01-11 22:54:20 +0100

    Update dress_test.rb

--- test/models/dress_test.rb
+++ test/models/dress_test.rb
@@ -1,71 +1,63 @@
 require 'test_helper'
 
 class DressTest < ActiveSupport::TestCase
-  test "the truth" do
-   assert true
-   end
-   
-   test "name can't be blank" do
-    assert_not Dress.create(name: "").valid?
-  end
-  
-  test "name should be saved" do
-    assert Dress.create(name: "Mala czarna").valid?
+  test "should save proper dress" do
+    dress = dresses(:one)
+    assert dress.save, "proper dress not saved"
   end
 
-  
-  test "should not save dress with too short name" do
-    assert_not Dress.create(name: "a").valid?
+  test "should not save dress without name" do
+    dress = dresses(:dress_without_name)
+    assert_not dress.save, "dress without name saved"
   end
- 
- 
-  test "should not save dress with an int in name" do
-      assert_not Dress.create(name: "123").valid?
+
+  test "should not save dress without cost" do
+    dress = dresses(:dress_without_cost)
+    assert_not dress.save, "dress without cost saved"
   end
-  
-   test "should not save dress with a string in cost" do
-     assert_not Dress.create(cost: "abcd").valid?
+
+  test "should not save dress without style" do
+    dress = dresses(:dress_without_style)
+    assert_not dress.save, "dress without style saved"
   end
-  
-   test "should  save dress with a decimal in cost" do
-     assert Dress.create(cost: "200.0").valid?
+
+  test "should not save dress without material" do
+    dress = dresses(:dress_without_material)
+    assert_not dress.save, "dress without material saved"
   end
-  
-     test "should not save dress with a negative number" do
-     assert_not Dress.create(cost: -1).valid?
+
+  test "should not save dress without order" do
+    dress = dresses(:dress_without_order)
+    assert_not dress.save, "dress without order saved"
   end
-   
-  
-  test "should not save dress with a zero value in cost" do
-     assert_not Dress.create(cost: 0).valid?
+
+  test "should not save dress with empty name" do
+    dress = dresses(:dress_with_empty_name)
+    assert_not dress.save, "dress with empty name saved"
   end
-  
-  test "cost can't be blank" do
-     assert_not Dress.create(cost:nil).valid?
+
+  test "should not save dress with empty cost" do
+    dress = dresses(:dress_with_empty_cost)
+    assert_not dress.save, "dress with empty cost saved"
   end
-  
-  test "should not save dress with too big value in cost" do
-     assert_not Dress.create(cost: 1000000.0).valid?
+
+  test "should not save dress with name too short" do
+    dress = dresses(:dresses_with_name_too_short)
+    assert_not dress.save, "dress with name too short saved"
   end
-  
-    test "should not save dress without style" do
-     assert_not Dress.create(style: nil).valid?
+
+  test "should not save dress with cost too low" do
+    dress = dresses(:dresses_with_cost_too_low)
+    assert_not dress.save, "dress with cost too low saved"
   end
-   test "should save dress with style" do
-     bufiasta=Styl.new
-     assert Dress.create(style: bufiasta).valid?
+
+  test "should not save dress with name too long" do
+    dress = dresses(:dresses_with_name_too_long)
+    assert_not dress.save, "dress with name too long saved"
   end
-  
-   test "should not save dress without material" do
-     assert_not Dress.create(style: nil).valid?
+
+  test "should not save dress with cost too big" do
+    dress = dresses(:dresses_with_cost_too_big)
+    assert_not dress.save, "dress with cost too big saved"
   end
-  
-   test "should save dress with material" do
-     jedwab=Material.new
-     assert_not Dress.create(style: jedwab).valid?
-  end
-  
-  
-  
-   
 end
