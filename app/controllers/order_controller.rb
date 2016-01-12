035afa6969d73fc4ab033792897578dd0e94b76f
Maciej Klemarczyk <m.klemarczyk@live.com>
2016-01-12 03:23:07 +0100

    Delivery and Confirm views were implemented. Confirm need more formatting.

--- app/controllers/order_controller.rb
+++ app/controllers/order_controller.rb
@@ -6,7 +6,7 @@ class OrderController < ApplicationController
     @dress = Dress.new()
 
 	if params[:dress] != nil
-      @dress = Dress.new(order_params)
+      @dress = Dress.new(dress_params)
       @dress.validate
       if @dress.errors[:style].any? or @dress.errors[:material].any?
         @errors = @dress.errors.full_messages
@@ -23,31 +23,46 @@ class OrderController < ApplicationController
   end
 
   def delivery
-    
-  end
+	@wszystkie_style = Style.all
+	@wszystkie_materialy = Material.all
 
-  def confirm
-    @dresses = []
-    pozycje = cookies[:zamowienie]
-    if pozycje != nil
-      pozycje = pozycje.split(';')
-      pozycje.each do |pozycja|
-        numery = pozycja.split(',')
-        if numery.count == 2 and numery[0].to_i > 0 and numery[1].to_i > 0
-          suknia = Dress.new()
-          suknia.style_id = numery[0].to_i
-          suknia.material_id = numery[1].to_i
-          if suknia.style != nil and suknia.material != nil
-            suknia.name = "#{suknia.style.name} - #{suknia.material.name}"
-            suknia.cost = (suknia.style.material_amount * suknia.material.price_per_m2).round(2)
-            @dresses << suknia
-          end
+    @client = Client.new
+    @client.address = Address.new
+
+	if params[:client] != nil
+      @client = Client.new(client_params)
+      @client.address = Address.new(address_params)
+      if(@client.address.flat.length == 0)
+        @client.address.flat = nil
+      end
+      if cookies[:klient] == nil
+        @client.save
+        if @client.errors.any?
+          @errors = @client.errors.full_messages
+        else
+          cookies[:klient] = @client.id
         end
+      end
+      if cookies[:klient] != nil
+        redirect_to "/potwierdzenie"
       end
     end
   end
 
-  def order_params
+  def confirm
+    if cookies[:klient] != nil
+      @order = Order.new
+      @order.client_id = cookies[:klient]
+    end
+  end
+
+  def dress_params
     params.require(:dress).permit(:style_id, :material_id)
+  end
+  def client_params
+    params.require(:client).permit(:firstname, :lastname, :phone, :address)
+  end
+  def address_params
+    params.require(:address).permit(:street, :building, :flat, :postalcode, :city)
   end
 end
