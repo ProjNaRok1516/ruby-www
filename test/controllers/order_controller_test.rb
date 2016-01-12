3dccc51db46f19d06e9191c2aac79610094aaea5
Maciej Klemarczyk <m.klemarczyk+git@live.com>
2016-01-12 13:30:27 +0100

    Update order_controller_test.rb

--- test/controllers/order_controller_test.rb
+++ test/controllers/order_controller_test.rb
@@ -8,12 +8,12 @@ class OrderControllerTest < ActionController::TestCase
 
   test "should get delivery" do
     get :delivery
-    assert_response :success
+    assert_response :302
   end
 
   test "should get confirm" do
     get :confirm
-    assert_response :success
+    assert_response :302
   end
 
 end
