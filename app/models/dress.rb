9479a4acb2e5b06406e5fa87578544f03b0af667
Maciej Klemarczyk <m.klemarczyk+git@live.com>
2016-01-12 01:07:11 +0100

    Update dress.rb

--- app/models/dress.rb
+++ app/models/dress.rb
@@ -2,11 +2,12 @@ class Dress < ActiveRecord::Base
   belongs_to :style
   belongs_to :material
   belongs_to :order
-  validates_associated :style
-  validates_associated :material
-  validates_associated :order
+
   
-   validates :name, :cost, presence: true
-   validates :name, length: { minimum: 3, maximum:50 } 
-   validates :cost, numericality: {greater_than: 0, less_than:5000}
+  validates :name, :cost, presence: true
+  validates :name, length: { minimum: 3, maximum: 50 } 
+  validates :cost, numericality: { greater_than: 0, less_than: 5000 }
+
+  validates_associated :style, :material, :order
+  validates_presence_of :style, :material, :order
 end
