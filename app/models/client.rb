94153cc46fef0f673c8fcbf687f78166cbb77998
Maciej Klemarczyk <m.klemarczyk+git@live.com>
2016-01-12 01:09:22 +0100

    Update client.rb

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,11 +1,12 @@
 class Client < ActiveRecord::Base
   belongs_to :address
+
+  validates :firstname, :lastname, :phone, presence: true
+  validates :firstname, :lastname, length: { minimum: 3, maximum: 50 } 
+  validates :phone, length: { is: 9 }
+  validates :phone, format: { with: /[1-9][0-9]{8}/, message: "allows only proper format od phone"}
+  validates :firstname, :lastname, format:  { with: /[A-Z][a-z]*/, message: "allows only capital letter at the beggining" }
+
   validates_associated :address
-   validates :firstname, :lastname, :phone, presence: true
-   validates :firstname, :lastname, length: { minimum: 3, maximum: 50 } 
-   validates :phone, length: { is: 9 }
-   validates :phone, format: { with: /[1-9][0-9]{8}/, message: "allows only proper format od phone"}
-   validates :firstname, :lastname, format:  { with: /[A-Z][a-z]*/, message: "allows only capital letter at the beggining" }
-   
-  
+  validates_presence_of :address
 end
