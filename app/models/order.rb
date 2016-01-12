7c9fd123e19281ee34d7dcee4ed3b4ffeda65c51
Maciej Klemarczyk <m.klemarczyk+git@live.com>
2016-01-12 01:21:07 +0100

    Data validator format was fixed.

--- app/models/order.rb
+++ app/models/order.rb
@@ -4,7 +4,7 @@ class Order < ActiveRecord::Base
   validates :data, :total_cost, :status, presence: true
   validates :total_cost, numericality: { greater_than: 0, less_than: 500000 }
   validates :status, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 1, only_integer: true }
-  validates :data, format: { with: /[0-3][0-9]\-[0-1][0-9]\-[2-9][0-9]{3}/ }
+  validates :data, format: { with: /[2-9]\d{3}\-[0-1]\d\-[0-3]\d\ [0-2]\d(\:[0-5]\d){2}/ }
 
   validates_associated :client
   validates_presence_of :client
